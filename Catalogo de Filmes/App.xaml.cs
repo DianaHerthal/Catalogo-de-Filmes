a159e9d7708f40379703b6d69688d1de6ab13772
Diana C. V. F. Herthal <94582628+DianaHerthal@users.noreply.github.com>
2026-06-09 20:26:38 -0300

    Adicionar arquivos de projeto.

---
+++ Catalogo de Filmes/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalogo_de_Filmes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Catalogo de Filmes/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalogo_de_Filmes
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
